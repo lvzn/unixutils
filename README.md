9c6577548143045fdffa86e0e0be9a507071e272
Leevi <joona.mlin@gmail.com>
2022-03-21 20:02:36 +0200

    added comments and stuff to readme

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # unixutils
+This repo our implementation of a few unix utilities:
+- cat
+## Compiling
+To compile the files type `make` in your unix shell
+## Usage
+### my-cat
+To use the program, run `./my-cat filename.txt` in your unix shell in the root of the folder. If multiple filenames are passed (e.g. `./my-cat filename1.txt filename2.txt filename3.txt`), the program will read them all in order, one by one. Filenames are separated with space.
--- my-cat.c
+++ my-cat.c
@@ -1,15 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+//Reading file given in args
 int readFile(char* filename) {
     FILE* f = NULL;
     char buf[100];
     int length = 100;
+    //Handling error in case when file cannot be opened
     if ((f = fopen(filename, "r")) == NULL) {
         fprintf(stderr, "my-cat: cannot open file\n");
         exit(1);
     }
-    while (fgets(buf, length, f) != NULL){
+    while (fgets(buf, length, f) != NULL){ //reading file line-by-line until fgets points to null
         printf("%s", buf);
     }
     puts("\n");
@@ -23,6 +26,7 @@ int main(int argc, char** argv) {
         exit(0);
         break;
     default:
+        //Initiating reading function to multiple files
         for (int i = 1; i < argc; i++) {
             readFile(argv[i]);
         }
